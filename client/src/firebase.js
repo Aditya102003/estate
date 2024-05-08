9954229905f67f511c73885cd06c82c2f46b66a8
unknown <apsharma202003@gmail.com>
2024-05-09 01:06:40 +0530

    adasd

---
+++ client/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-Ful9TTsKgFSCXG4hVGnKTLwHp2vFH-8",
+  authDomain: "esate-e5957.firebaseapp.com",
+  projectId: "esate-e5957",
+  storageBucket: "esate-e5957.appspot.com",
+  messagingSenderId: "157608282124",
+  appId: "1:157608282124:web:d2cf8f6fc0481ef9b68629",
+  measurementId: "G-PCYKM41C3N"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
